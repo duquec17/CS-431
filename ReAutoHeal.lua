6bf752025a34836be9a29038714b62cea1bd951c
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-05 13:31:55 -0500

    Rewrite to Match Lua syntax

--- ReAutoHeal.lua
+++ ReAutoHeal.lua
@@ -6,95 +6,95 @@ local ManaWarn = 50
 
 -- Check if a target is selected
 if not mq.TLO.Target.ID() then
-    mq.cmd('/say No target selected, exiting macro.')
+    mq.cmd.Say('No target selected, exiting macro.')
     return
 end
 
 -- Loads spells when starting
-mq.cmd('/say memorizing spells')
+mq.cmd.Say('Memorizing spells')
 mq.cmd.MemSpellSet('autoHeal20')
 mq.cmd.Delay(5)
 
 -- Start of macro
-mq.Echo()
-mq.Echo("start of macro")
-mq.Say("${Group.Member[1]} is the tank")
+print("")
+print("start of macro")
+mq.cmd.Say("${Group.Member[1]} is the tank")
 
 -- Create While loop that ends when macro is ended
 while true do
 
     -- Check current conditions
-    if mq.TLO.Group.Member[1].PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 or mq.TLO.Group.Member[2].PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 then
+    if mq.TLO.Group.Member(1).PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 or mq.TLO.Group.Member(2).PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 then
         goto buff1
     end
 
     -- Heal check
-    mq.Echo("Running heal check")
-    if mq.TLO.Group.Member[1].PctHPs() < 85 and mq.TLO.Group.Member[1].PctHPs() > 65 or mq.TLO.Group.Member[2].PctHPs() < 85 and mq.TLO.Group.Member[2].PctHPs() > 65 then
+    print("Running heal check")
+    if mq.TLO.Group.Member(1).PctHPs() < 85 and mq.TLO.Group.Member(1).PctHPs() > 65 or mq.TLO.Group.Member(2).PctHPs() < 85 and mq.TLO.Group.Member(2).PctHPs() > 65 then
         goto heal1
-    elseif mq.TLO.Group.Member[1].PctHPs() <= 65 then
+    elseif mq.TLO.Group.Member(1).PctHPs() <= 65 then
         goto heal2
     elseif mq.TLO.Me.PctMana() < 100 then
-        mq.Say("My mana is at " .. mq.TLO.Me.PctMana() .. " percent.")
+        mq.cmd.Say("My mana is at " .. mq.TLO.Me.PctMana() .. " percent.")
     end
-    mq.Echo("end of heal check")
+    print("end of heal check")
 
     ::endCheck::
-    mq.Delay(5)
+    mq.cmd.Delay(5)
 
     ::loop::
 end
 
 ::heal1::
-mq.Say("casting minor healing")
-mq.Target("${Group.Member[1]}")
-mq.Cast(2)
+mq.cmd.Say("casting minor healing")
+mq.cmd.Target(mq.TLO.Group.Member(1))
+mq.cmd.Cast(2)
 mq.Delay(3)
 if mq.TLO.Me.Standing() and not mq.TLO.Me.Mount.ID() then
-    mq.Sit()
-    mq.Echo("should be sitting")
+    mq.cmd.Sit()
+    print("should be sitting")
 end
-mq.Delay(3)
+mq.cmd.Delay(3)
 goto body
 
 ::heal2::
-mq.Say("casting healing")
-mq.Target("${Group.Member[1]}")
-mq.Cast(1)
-mq.Delay(3)
+mq.cmd.Say("casting healing")
+mq.cmd.Target("${Group.Member[1]}")
+mq.cmd.Cast(1)
+mq.cmd.Delay(3)
 if mq.TLO.Me.Standing() and not mq.TLO.Me.Mount.ID() then
-    mq.Sit()
+    mq.cmd.Sit()
     mq.Echo("should be sitting")
 end
-mq.Delay(3)
+mq.cmd.Delay(3)
 goto body
 
 ::buff1::
 -- Check if Spirit Armor buff is already applied
-if mq.TLO.Target.Buff["Spirit Armor"].ID() then
-    mq.Echo("Armor buff already applied")
+if mq.TLO.Target.Buff('Spirit Armor').ID() then
+    print("Armor buff already applied")
     goto healCheck
 end
 for i = 1, 2 do
-    mq.Target("${Group.Member[i]}")
-    mq.Say("it's Buff time " .. mq.TLO.Group.Member[i])
-    mq.Cast(3)
-    mq.Delay(5)
+    mq.cmd.Target(mq.TLO.Group.Member(i))
+    --mq.cmd.Say("Armor buff  " .. mq.TLO.Group.Member(i))
+    mq.cmd.Cast(3)
+    mq.cmd.Delay(5)
 end
 goto buff2
 -- Returns to healCheck loop --
 
 -- Check if Blessing is already applied --
 ::buff2::
-if mq.TLO.Target.Buff["Blessing of Piety"].ID() then
-    mq.Echo("Blessing buff already applied")
+if mq.TLO.Target.Buff('Blessing of Piety').ID() then
+    print("Blessing buff already applied")
     goto healCheck
 end
 for i = 1, 2 do
-    mq.Target("${Group.Member[i]}")
-    mq.Say("it's Buff time " .. mq.TLO.Group.Member[i])
-    mq.Cast(4)
-    mq.Delay(5)
+    mq.cmd.Target(mq.TLO.Group.Member(i))
+    --mq.cmd.Say("Blessing buff " .. mq.TLO.Group.Member(i))
+    mq.cmd.Cast(4)
+    mq.cmd.Delay(5)
 end
 goto healCheck
 -- Returns to healCheck loop --
@@ -110,5 +110,5 @@ goto healCheck
 -- Placeholder for code logic or conditions
 
 -- End of macro
-mq.Echo("The macro is ending")
+print("The macro is ending")
 return
