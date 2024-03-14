209a360e4bb6c7db3bab7e13f6771feb9902d2a0
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-14 11:38:40 -0400

    Update ReAutoHeal.lua

--- ReAutoHeal.lua
+++ ReAutoHeal.lua
@@ -6,10 +6,7 @@ local ManaWarn = 50
 local terminate = false;
 
 -- Check if a target is selected
-if not mq.TLO.Target.ID() then
-    mq.cmd.Say('No target selected, exiting macro.')
-    return
-end
+
 
 -- Loads spells when starting
 mq.cmd.Say('Memorizing spells')
@@ -23,13 +20,17 @@ print("start of macro")
 -- All functions definitions --
 -- Heal function 1 when health is slightly low --
 local function heal1()
-    print("casting healing")
-    mq.cmd.Target(mq.TLO.Group.Member(1))
+    --print("casting healing")
+    if mq.TLO.Group.Member(1).PctHPs() < 85 then
+        mq.cmd.Target(mq.TLO.Group.Member(1))
+    elseif mq.TLO.Group.Member(2).PctHPs() < 85 then
+        mq.cmd.Target(mq.TLO.Group.Member(2))
+    end
+    
     mq.cmd.Cast(2)
     mq.delay(3000)
     if mq.TLO.Me.Standing() and not mq.TLO.Me.Mount.ID() then
         mq.cmd.Sit()
-        print("should be sitting")
     end
     mq.delay(5000)
     print("end of heal 1")
@@ -37,13 +38,17 @@ end
 
 -- Heal function 2 when health is very low --
 local function heal2()
-    print("casting Greater healing")
-    mq.cmd.Target("${Group.Member[1]}")
+    --print("casting Greater healing")
+    if mq.TLO.Group.Member(1).PctHPs() <= 65 then
+        mq.cmd.Target(mq.TLO.Group.Member(1))
+    elseif mq.TLO.Group.Member(2).PctHPs() <= 65 then
+        mq.cmd.Target(mq.TLO.Group.Member(2))
+    end
+
     mq.cmd.Cast(1)
     mq.delay(3000)
     if mq.TLO.Me.Standing() and not mq.TLO.Me.Mount.ID() then
         mq.cmd.Sit()
-        print("should be sitting")
     end
     mq.delay(3000)
     print("end of heal 2")
@@ -57,13 +62,11 @@ local function buff1()
     else
         for i = 1, 2 do
             mq.cmd.Target(mq.TLO.Group.Member(i))
-            print("Armor buff for ally ")
             mq.cmd.Cast(3)
             mq.delay(8000)
         end
     end
    
-    print("end of buff 1")
 end
 
 -- Buff function 2 that checks and applies 2nd buff --
@@ -73,13 +76,11 @@ local function buff2()
     else
         for i = 1, 2 do
             mq.cmd.Target(mq.TLO.Group.Member(i))
-            print("Strength buff for ally")
             mq.cmd.Cast(4)
             mq.delay(10000)
         end
     end
     
-    print("end of buff 2")
 end
 
 -- Buff function 3 that checks and applies 3rd buff --
@@ -89,13 +90,11 @@ local function buff3()
     else
         for i = 1, 2 do
             mq.cmd.Target(mq.TLO.Group.Member(i))
-            mq.cmd.Say("Symbol buff for 'i' ")
             mq.cmd.Cast(5)
             mq.delay(7000)
         end
     end
-    
-    print("end of buff 3")
+
 end
 
 -- Buff function 4 that checks and applies 3rd buff --
@@ -105,29 +104,24 @@ local function buff4()
     else
         for i = 1, 2 do
             mq.cmd.Target(mq.TLO.Group.Member(i))
-            mq.cmd.Say("Shield buff for 'i' ")
             mq.cmd.Cast(6)
             mq.delay(6000)
         end
     end
 
-    print("end of buff 4")
 end
 
 -- Stun debuff function 1 when allies HP 85+, Mana 60+, and  buffs true --
 local function stun1()
    -- Target the tank's target if it exists, otherwise target the last hitter
    local tank = mq.TLO.Group.Member(1)
-   printf('%s is the tank', tank)
     
    mq.cmd.assist(tank) -- Targets the tank's current target
 
    mq.cmd.Cast(7)  -- Assuming spell ID 7 is for stunning
-   print("Stunning enemy")
    mq.delay(6000)
 
    mq.cmd.Cast(8)  -- Assuming spell ID 8 is for damaging
-   print("Damaging enemy")
    mq.delay(6000)
 end
 
@@ -138,22 +132,26 @@ while not terminate
 do
     -- Check Buff current conditions
     if mq.TLO.Group.Member(1).PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 and mq.TLO.Group.Member(2).PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 75 then
-        print("Heading to buff1")
         buff1()
         buff2()
         buff3()
         buff4()
+
+        --print("Moving to Heal Check")
+        
+        if mq.TLO.Me.Standing() and not mq.TLO.Me.Mount.ID() then
+            mq.cmd.Sit()
+        end
+        mq.delay(3000)
     end
 
     -- Heal check --
-    print("Running heal check")
+    --print("Running heal check")
     if mq.TLO.Group.Member(1).PctHPs() < 85 and mq.TLO.Group.Member(1).PctHPs() > 65 or mq.TLO.Group.Member(2).PctHPs() < 85 and mq.TLO.Group.Member(2).PctHPs() > 65 then
-        print("Heading to heal 1")
         heal1()
     elseif mq.TLO.Group.Member(1).PctHPs() <= 65 or mq.TLO.Group.Member(2).PctHPs() <= 65 then
-        print("Heading to heal 2")
         heal2()
-    elseif mq.TLO.Group.Member(1).PctHPs() == 100 and mq.TLO.Me.PctManaMana() == 100 then
+    elseif mq.TLO.Group.Member(1).PctHPs() == 100 and mq.TLO.Me.PctMana() == 100 then
         print("All characters at full health")
         terminate = true;
     end
@@ -162,20 +160,16 @@ do
     -- Debuff/DMG check -- 
     if mq.TLO.Group.Member(1).PctHPs() >= 85 and mq.TLO.Me.PctMana() >= 60 then
         stun1()
-    else
-        print("Not enough resources to debuff")
+    elseif mq.TLO.Me.PctMana() <= 60 then
+        --print("Not enough resources to debuff")
     end
 
     -- Mana call out --
     if mq.TLO.Me.PctMana() <= 30 then
         mq.cmd.Say("My mana is at " .. mq.TLO.Me.PctMana() .. " percent.")
-
         mq.cmd.Sit()
-        print("should be sitting")
         terminate = true;
     end
     -- Checks to see whether to terminate script --
-
-    print("end of loop")
 end
 return
